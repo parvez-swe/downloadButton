428ad830f0e5748d8b67da2a2dc83464e01d194c
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-12-11 19:25:34 +0600

    class at image

--- components/Download.js
+++ components/Download.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Download = () => {
   return (
     <div className="mb-20">
-      <img src="/image.png" alt="image" />
+      <img className="" src="/image.png" alt="image" />
       <a className="bg-red-500 text-white rounded px-5 py-2" href="/image.png" download>
         Download Image
       </a>
