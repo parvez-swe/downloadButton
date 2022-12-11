86703921f1c45b33f85388f54970061ce5e2dc6b
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-12-11 19:24:53 +0600

    first commit

--- components/Download.js
+++ components/Download.js
@@ -2,14 +2,12 @@ import React from "react";
 import Image from "next/image";
 const Download = () => {
   return (
-    <div>
-    <div className="">
-      <Image className="mb-5" src="/image.png" height={250} width={250} />
-      <a className="bg-red-500 px-2 py-1 rounded text-white" href="/image.png" download>Download Image</a>
-    </div>
-    <div className="my-10">
-    <a href="/cv.pdf" className="bg-red-500 px-2 py-1 rounded text-white" download>Download CV</a>
-    </div>
+    <div className="mb-20">
+      <img src="/image.png" alt="image" />
+      <a className="bg-red-500 text-white rounded px-5 py-2" href="/image.png" download>
+        Download Image
+      </a>
+
     </div>
   );
 };
